ee67045d584533ef0e5a6df63388698af4c0a336
Neverender <tldavis09@yahoo.com>
2020-07-06 22:58:11 -0500

    working on formatting before adding actual content

--- src/components/AppContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AppContainer = () => {
-	return <div className="app-container"></div>;
-};
-
-export default AppContainer;
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFile as faFileAlt } from "@fortawesome/free-solid-svg-icons";
 import { faFile } from "@fortawesome/free-regular-svg-icons";
 
 const SideNav = () => {
 	return (
-		<ul className="side-nav">
-			<li>
-				<FontAwesomeIcon icon={faGithub} /> Github
-			</li>
-			<li>
-				<FontAwesomeIcon icon={faLinkedin} /> Linkedin
-			</li>
-			<li>
-				<FontAwesomeIcon icon={faFile} /> Resume
-			</li>
-		</ul>
+		<div className="side-nav">
+			<ul>
+				<li>
+					<FontAwesomeIcon icon={faGithub} /> Github
+				</li>
+				<li>
+					<FontAwesomeIcon icon={faLinkedin} /> Linkedin
+				</li>
+				<li>
+					<FontAwesomeIcon icon={faFile} /> Resume
+				</li>
+			</ul>
+		</div>
 	);
 };
 
