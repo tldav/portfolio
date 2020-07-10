a5066bbe779900d866ebc3141dd32fa5d3745faf
Neverender <tldavis09@yahoo.com>
2020-07-09 21:11:26 -0500

    refactored form to functional component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-// const Contact = () => {
-// 	return (
-// 		<div>
-// 			<h1>juggalo</h1>
-// 		</div>
-// 	);
-// };
+const Contact = () => {
+	const [status, setStatus] = useState("");
 
-// export default Contact;
-
-class Contact extends React.Component {
-	constructor(props) {
-		super(props);
-		this.submitForm = this.submitForm.bind(this);
-		this.state = {
-			status: ""
-		};
-	}
-
-	render() {
-		const { status } = this.state;
-		return (
-			<form
-				onSubmit={this.submitForm}
-				action="https://formspree.io/xleppyja"
-				method="POST"
-			>
-				<label>Email:</label>
-				<input type="email" name="email" />
-				<label>Message:</label>
-				<input type="text" name="message" />
-				{status === "SUCCESS" ? (
-					<p>Thanks!</p>
-				) : (
-					<button>Submit</button>
-				)}
-				{status === "ERROR" && <p>Ooops! There was an error.</p>}
-			</form>
-		);
-	}
-
-	submitForm(ev) {
+	const submitForm = (ev) => {
 		ev.preventDefault();
 		const form = ev.target;
 		const data = new FormData(form);
@@ -52,13 +14,28 @@ class Contact extends React.Component {
 			if (xhr.readyState !== XMLHttpRequest.DONE) return;
 			if (xhr.status === 200) {
 				form.reset();
-				this.setState({ status: "SUCCESS" });
+				setStatus("SUCCESS");
 			} else {
-				this.setState({ status: "ERROR" });
+				setStatus("ERROR");
 			}
 		};
 		xhr.send(data);
-	}
-}
+	};
+
+	return (
+		<form
+			onSubmit={submitForm}
+			action="https://formspree.io/xleppyja"
+			method="POST"
+		>
+			<label>Email:</label>
+			<input type="email" name="email" />
+			<label>Message:</label>
+			<input type="text" name="message" />
+			{status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+			{status === "ERROR" && <p>Ooops! There was an error.</p>}
+		</form>
+	);
+};
 
 export default Contact;
