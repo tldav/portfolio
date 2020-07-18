bb22a303132fda8867424ed7908517c265a57fe9
Neverender <tldavis09@yahoo.com>
2020-07-17 21:37:02 -0500

    the project display is formatted the way I want. Still need to figure out a solution for the difficult to read plumm.it title text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,85 +1,34 @@
 import React from "react";
+import "../stylesheets/Projects.css";
 import projectInfo from "../project-info.json";
 
 const Projects = () => {
 	return (
 		<div className="projects">
-			{/* <img src={projectInfo[0].image} alt="" />
-			<img src={projectInfo[1].image} alt="" />
-			<img src={projectInfo[2].image} alt="" />
-			<img src={projectInfo[3].image} alt="" /> */}
-
-			<div className="proj-container">
-				<div className="content">
-					<div className="content-overlay"></div>
-					<img
-						className="content-image"
-						src="assets/alt-space-360.jpg"
-					/>
-					<h1 className="proj-title yoda-title">YodaScope</h1>
-					<div className="content-details fadeIn-bottom">
-						<h3 className="content-title">This is a title</h3>
-						<p className="content-text">
-							This is a short description
-						</p>
+			{projectInfo.map((project) => {
+				return (
+					<div className="proj-container">
+						<div className="content">
+							<div className="content-overlay"></div>
+							<img
+								className="content-image"
+								src={project.image}
+							/>
+							<h1 className="proj-title" id={project.css}>
+								{project.name}
+							</h1>
+							<div className="content-details fadeIn-bottom">
+								<h3 className="content-title">
+									This is a title
+								</h3>
+								<p className="content-text">
+									{project.description}
+								</p>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-
-			<div className="proj-container">
-				<div className="content">
-					<div className="content-overlay"></div>
-					<img
-						className="content-image"
-						src="assets/alt-plum-360.jpg"
-					/>
-					<h1 className="proj-title plumm-title">plumm.it</h1>
-					<div className="content-details fadeIn-bottom">
-						<h3 className="content-title">This is a title</h3>
-						<p className="content-text">
-							A forum with user signup and authentication. Created
-							using JavaScript, jQuery, Node, Express, and MySQL
-							with Sequelize.
-						</p>
-					</div>
-				</div>
-			</div>
-
-			<div className="proj-container">
-				<div className="content">
-					<div className="content-overlay"></div>
-					<img
-						className="content-image"
-						src="assets/alt-fit-360.jpg"
-					/>
-					<h1 className="proj-title fit-title">Fit Tracker</h1>
-					<div className="content-details fadeIn-bottom">
-						<h3 className="content-title">This is a title</h3>
-						<p className="content-text">
-							This is a short description
-						</p>
-					</div>
-				</div>
-			</div>
-
-			<div className="proj-container">
-				<div className="content">
-					<div className="content-overlay"></div>
-					<img
-						className="content-image"
-						src="assets/alt-charity-360.jpg"
-					/>
-					<h1 className="proj-title charity-title">
-						Charity Tracker
-					</h1>
-					<div className="content-details fadeIn-bottom">
-						<h3 className="content-title">This is a title</h3>
-						<p className="content-text">
-							This is a short description
-						</p>
-					</div>
-				</div>
-			</div>
+				);
+			})}
 		</div>
 	);
 };
