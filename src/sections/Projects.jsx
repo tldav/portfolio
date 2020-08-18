205f03a72f2788deaa043588b37b4ec4f8b41ced
Neverender <tldavis09@yahoo.com>
2020-08-17 23:40:07 -0500

    added project hover formats for smaller screens and changed json to js file for project info

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import "../stylesheets/Projects.css";
-import projectInfo from "../project-info.json";
+import projectInfo from "../project-info.js";
 
 const Projects = () => {
 	const githubLink = (
